77399adc2c4da70b64797d6d1067cb9d282d957c
shin-ito0306 <shin.20000306@icloud.com>
2021-05-23 04:29:37 +0000

    [update] login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_sign_in_params, only: [:create]
     
     protected
     
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+    
+    def configure_sign_in_params
+       devise_parameter_sanitizer.permit(:sign_in, keys: [:name]) 
     end
 end
