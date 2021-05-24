649a59454dc182413125d19c36e53d7b622153c4
shin-ito0306 <shin.20000306@icloud.com>
2021-05-24 05:15:32 +0000

    [add]validate, flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "successfully"
+    else
+      @user = User.find(current_user.id)
+      @books = Book.all
+      render :index
+    end
   end
   
   def edit
@@ -29,8 +35,12 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "successfully"
+    else
+      render :edit
+    end
   end
   
   def destroy
