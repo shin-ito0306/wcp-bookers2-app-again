18147a121fa9c44828306e6f7e73b90ea8490363
shin-ito0306 <shin.20000306@icloud.com>
2021-05-24 06:49:51 +0000

    [update] attachment_image_tag

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '48703d11e1efc8b4a6bf18f6873779df79834ebbe2f73fa98d4ba9a28de36767c3ccd91d013606a5bbd651d8640804994bc4654c25c852d981d354cde1f6e5d2'
