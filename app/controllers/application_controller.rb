1f15bc312a31abc7c3f73c33525e1d9e9b6aaa11
shin-ito0306 <shin.20000306@icloud.com>
2021-05-24 01:11:46 +0000

    [add] book_show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :configure_sign_in_params, only: [:create]
+   
     
     protected
     
@@ -8,7 +8,5 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
     
-    def configure_sign_in_params
-       devise_parameter_sanitizer.permit(:sign_in, keys: [:name]) 
-    end
+   
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+  
+  def index
+    @user = User.find(current_user.id)
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def show
+    @book_show = Book.find(params[:id])
+    @book = Book.new
+    @user = User.find(current_user.id)
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  
+  private
+  
+    def book_params
+      params.require(:book).permit(:title, :body, :user_id)
+    end
+  
 end
