323d5f998e318c866119dc9ae56f50fb0219d8bd
shin-ito0306 <shin.20000306@icloud.com>
2021-05-24 23:31:58 +0000

    [finish] bookers2-againgit add .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
   
   def update
