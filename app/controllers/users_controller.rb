113612e00ec3ba04bfbc7ec09e9c160e1b24e041
shin-ito0306 <shin.20000306@icloud.com>
2021-05-24 01:18:19 +0000

    [add] user_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.all
+    @book = Book.new
+    @user = User.find(current_user.id)
+  end
   
   def edit
     @user = User.find(params[:id])
